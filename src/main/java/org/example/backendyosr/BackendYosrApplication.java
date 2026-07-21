1459f4e39144e0b873432f439afc755b6d4303e2
yosr2003 <yosr.amamou@yahoo.com>
2026-07-21 09:10:48 +0100

    Initial Spring Boot backend

---
+++ src/main/java/org/example/backendyosr/BackendYosrApplication.java
@@ -0,0 +1,13 @@
+package org.example.backendyosr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendYosrApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackendYosrApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/backendyosr/BackendYosrApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.backendyosr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendYosrApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
